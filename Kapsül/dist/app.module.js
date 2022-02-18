a53446e314c8e58269328d0bdc9ebc03a3d827e5
Hasan Hüseyin <hhsonmez68@gmail.com>
2022-02-18 22:41:26 +0300

    Sql configuration
    type orm mssql configuration created.

--- Kapsül/dist/app.module.js
+++ Kapsül/dist/app.module.js
@@ -8,13 +8,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const mssql_config_1 = require("./config/mssql.config");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [typeorm_1.TypeOrmModule.forRoot(mssql_config_1.configService.getTypeOrmConfig())],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
---
+++ Kapsül/dist/config/mssql.config.js
@@ -0,0 +1,55 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.configService = void 0;
+require('dotenv').config();
+console.log(process.env.MSSQL_HOST);
+class ConfigService {
+    constructor(env) {
+        this.env = env;
+    }
+    getValue(key, throwOnMissing = true) {
+        const value = this.env[key];
+        if (!value && throwOnMissing) {
+            throw new Error(`config error - missing env.${key}`);
+        }
+        return value;
+    }
+    ensureValues(keys) {
+        keys.forEach(k => this.getValue(k, true));
+        return this;
+    }
+    getPort() {
+        return this.getValue('PORT', true);
+    }
+    isProduction() {
+        const mode = this.getValue('MODE', false);
+        return mode != 'DEV';
+    }
+    getTypeOrmConfig() {
+        return {
+            type: 'mssql',
+            host: this.getValue('MSSQL_HOST'),
+            port: parseInt(this.getValue('MSSQL_PORT')),
+            username: this.getValue('MSSQL_USER'),
+            password: this.getValue('MSSQL_PASSWORD'),
+            database: this.getValue('MSSQL_DATABASE'),
+            entities: ['**/*.entity{.ts,.js}'],
+            migrationsTableName: 'migration',
+            migrations: ['src/migration/*.ts'],
+            cli: {
+                migrationsDir: 'src/migration',
+            },
+            ssl: this.isProduction(),
+        };
+    }
+}
+const configService = new ConfigService(process.env)
+    .ensureValues([
+    'MSSQL_HOST',
+    'MSSQL_PORT',
+    'MSSQL_USER',
+    'MSSQL_PASSWORD',
+    'MSSQL_DATABASE'
+]);
+exports.configService = configService;
+//# sourceMappingURL=mssql.config.js.map
